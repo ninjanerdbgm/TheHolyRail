9e8918d643fa6753d3d8ef264d10b9a69ef49edd
Matt Formica <matt@formica.dev>
2023-03-11 14:03:07 -0800

    - Renamed "Powered Rail" to "Powered Track" to be more in line with the game's naming scheme. - Fixed bug with powered TRACKS that disallowed corner and junction connections - Added the "Track Torpedo" item, a faster minecart.

---
+++ src/main/java/theholyrailmod/patch/MinecartMobPatch.java
@@ -0,0 +1,46 @@
+package theholyrailmod.patch;
+
+import necesse.engine.modLoader.annotations.ModMethodPatch;
+import necesse.entity.mobs.Mob;
+import necesse.entity.mobs.summon.MinecartMob;
+import necesse.level.gameObject.GameObject;
+import net.bytebuddy.asm.Advice;
+
+import theholyrailmod.theholyrail.PoweredRailObject;
+import theholyrailmod.theholyrail.RailRunnerMob;
+
+public class MinecartMobPatch {
+    @ModMethodPatch(target = MinecartMob.class, name = "tickCollisionMovement", arguments = {float.class, Mob.class})
+    public static class TickCollisionMovementPatch {
+        @Advice.OnMethodEnter
+        public static void onEnter(@Advice.This MinecartMob obj, @Advice.Argument(0) float delta, @Advice.Argument(1) Mob rider) {            
+            int tileX = obj.getTileX();
+            int tileY = obj.getTileY();
+            GameObject object = obj.getLevel().getObject(tileX, tileY);        
+
+            if (!(obj instanceof RailRunnerMob)) {
+                if (object instanceof PoweredRailObject) {
+                    if (((PoweredRailObject)object).isPowered(obj.getLevel(), tileX, tileY) && rider != null) {
+                        obj.minecartSpeed = Math.min(200.0f, obj.minecartSpeed + 5.7f);
+                    } else if (rider == null) {
+                        obj.minecartSpeed = 0.0f;
+                    } else {
+                        obj.minecartSpeed = Math.min(4f, obj.minecartSpeed > 4f ? obj.minecartSpeed / 2.0f : obj.minecartSpeed - 0.01f);
+                    }
+                }
+            } else {
+                if (object instanceof PoweredRailObject) {
+                    if (((PoweredRailObject)object).isPowered(obj.getLevel(), tileX, tileY) && rider != null) {
+                        obj.minecartSpeed = Math.min(262.0f, obj.minecartSpeed + 9.2f);
+                    } else if (rider == null) {
+                        obj.minecartSpeed = 0.0f;
+                    } else {
+                        obj.minecartSpeed = Math.min(12f, obj.minecartSpeed > 12f ? obj.minecartSpeed / 2.0f : obj.minecartSpeed - 0.01f);
+                    }
+                }
+            }
+                    
+            obj.sendMovementPacket(false);  
+        }        
+    }
+}
